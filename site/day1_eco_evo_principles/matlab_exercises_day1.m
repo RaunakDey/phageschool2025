0e4c4943cec2f1fa9d2fd529413e7e5ab6107944
RaunakDey <deyraunak111@gmail.com>
2025-06-22 21:02:34 -0400

    python day1

---
+++ site/day1_eco_evo_principles/matlab_exercises_day1.m
@@ -0,0 +1,11 @@
+%% Simulate a viral invasion - SV lytic model
+
+%% TO BE DONE LATER
+
+% This script demonstrates the function $\sin(x)$ and its LaTeX rendering.
+% Consider the SV model for lytic infections with susceptible cells (S) and free viruses (V) given by,
+% $$ \dot{S} = bS $$
+% $$ \dot{S} = bS $$
+
+S = 1:3;
+plot(S,S,'-ro');
